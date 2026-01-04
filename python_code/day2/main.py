afbee56e82d7b3c1775851867b8dcbd97d83fbf4
Maggo Mor <maggomorizon@gmail.com>
2026-01-04 12:36:37 +0100

    Solutions days 2 and 3

---
+++ python_code/day2/main.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel
+import numpy as np
+import itertools
+
+class DayTwoClass(BaseModel):
+    product_ids: str
+    
+    def get_ordered_list_of_ids(self) -> list:
+        ids_list = self.product_ids.split(",")
+        ordered_list = [x.split("-") for x in ids_list]
+        return ordered_list
+    
+    def check_repeating_numbers(self, list_input: list) -> int:
+        invalid_ids = []
+        for number in range(int(list_input[0]), int(list_input[1])+1):
+            number_str = str(number)
+            if len(number_str) % 2 != 0:
+                pass
+            else:
+                for increment in range(1,len(number_str)+1):
+                    if number_str[0:increment] * int((len(number_str) / increment)) == number_str:
+                        if int(len(number_str) / increment) == 2:
+                            print(number_str)
+                            invalid_ids.append(int(number_str))
+        return invalid_ids 
+    
+    def process_product_ids(self) -> int:
+        all_invalid_ids = []
+        ordered_lists = self.get_ordered_list_of_ids()
+        for number_pair in ordered_lists:
+            print(number_pair)
+            all_invalid_ids.append(self.check_repeating_numbers(number_pair))
+        flattened_list = list(itertools.chain(*all_invalid_ids))
+        sum_number = np.sum(np.array(flattened_list))
+        return int(sum_number)
+
+
+if __name__ == "__main__":
+    product_ids = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+    checker = DayTwoClass(product_ids = product_ids)
+    outcome = checker.process_product_ids()
+    print(f"Sum of all invalid IDs is: {outcome}")
---
+++ python_code/day3/main.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+import numpy as np
+
+def argmax(iterable):
+    return max(enumerate(iterable), key=lambda x: x[1])[0]
+
+class DayThreeClass(BaseModel):
+    joltage_ratings: str
+    
+    def get_list_of_joltages(self) -> list:
+        joltage_ratings_list = self.joltage_ratings.split("\n")
+        return joltage_ratings_list
+    
+    def list_battery_bank(self, battery_input: str) -> list:
+        return [int(battery_input[num]) for num in range(len(battery_input))]
+    
+    def process_battery_bank(self, battery_input: str) -> int:
+        result = self.list_battery_bank(battery_input)
+        first_val = argmax(result[:-1])
+        second_val = first_val + 1 + argmax(result[first_val+1:])
+        return int(str(result[first_val]) + str(result[second_val]))
+    
+    def process_joltage_ratings(self) -> int:
+        value = 0
+        joltage_ratings_list = self.get_list_of_joltages()
+        for joltage_rating in joltage_ratings_list:
+            value += self.process_battery_bank(joltage_rating)
+        return value
+
+if __name__ == "__main__":
+    joltage_ratings = "987654321111111\n811111111111119\n234234234234278\n818181911112111"
+    checker = DayThreeClass(joltage_ratings = joltage_ratings)
+    outcome = checker.process_joltage_ratings()
+    print(f"Sum of Joltages is: {outcome}")
