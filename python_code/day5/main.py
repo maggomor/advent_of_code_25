fbe5970b2b0df37c0b89c7941c5a8e11019ef58e
Maggo Mor <maggomorizon@gmail.com>
2026-01-04 17:52:06 +0100

    Solved Day Five

---
+++ python_code/day5/main.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+import numpy as np
+from typing import Tuple
+
+class DayFiveClass(BaseModel):
+    puzzle_input: str
+    
+    def process_puzzle_input(self) -> Tuple[list, list]:
+        return self.puzzle_input.split("\n\n")
+    
+    def get_list_of_ranges(self, input_list: list) -> list:
+        return [range(int(x.split("-")[0]), int(x.split("-")[1])+1) for x in input_list.split("\n")]
+
+    def check_all_ingredients(self) -> int:
+        counter = 0
+        ranges_str, idx = self.process_puzzle_input()
+        ranges = self.get_list_of_ranges(ranges_str)
+        for elem in idx.split("\n"):
+            id_value = int(elem)
+            flag = False
+            for range_elem in ranges:
+                if (id_value in range_elem and not flag):
+                    counter +=1
+                    flag = True
+        return counter
+    
+if __name__ == "__main__":
+    puzzle_input = "3-5\n10-14\n16-20\n12-18\n\n1\n5\n8\n11\n17\n32"
+    checker = DayFiveClass(puzzle_input = puzzle_input)
+    outcome = checker.check_all_ingredients()
+    print(f"Number of acceptable ingredients is: {outcome}")
